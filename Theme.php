8ab296d538c7c505fe11ca4e256bbf405e39169f
Gustavo Lima da Silva <gustavo.silva.vs@gmail.com>
2016-06-23 19:34:07 -0300

    Update assets and header

--- Theme.php
+++ Theme.php
@@ -16,67 +16,23 @@ class Theme extends BaseV1\Theme{
         return [
             'site: name' => App::i()->config['app.siteName'],
             'site: description' => App::i()->config['app.siteDescription'],
-
             'site: in the region' => 'na região',
-
             'site: of the region' => 'da região',
+            'site: owner' => 'Secretaria',
+            'site: by the site owner' => 'pela Secretaria',
 
-            'site: owner' => 'Secretaria de Estado da Cultura do Mato Grosso',
-
-            'site: by the site owner' => 'pela Secretaria de Estado da Cultura do Mato Grosso',
-
-            'home: title' => "Bem-vind@!",
-
-            'home: abbreviation' => "Cultura MT",
-
-            'home: colabore' => "Colabore com o Mapas Culturais",
-
-            'home: welcome' => "Mapas Culturais é um software livre para mapeamento colaborativo
-				 da cultura. O sistema é alimentado tanto pela Secretaria de
-				 Cultura, que insere na plataforma informações sobre os 
-				equipamentos culturais, programações oficiais, editais, e outros,
-				 como pela população em geral, que se cadastra como agente de 
-				cultura (individual ou coletivo) e pode divulgar suas próprias
-				 programações. Esta ferramenta incorpora nos processos públicos
-				 as lógicas do software livre, da colaboração, da
-				 descentralização, do uso de dados abertos e da transparência.
-				 Além disso, fortalece o Plano Nacional e Estadual de Cultura,
-				 contribuindo para o seu cumprimento e acompanhamento de algumas
-				 de suas metas. Participe!",
-
-            'home: events' => "Veja a agenda de eventos culturais da sua cidade. Como usuário
-				 cadastrado você pode incluir seus eventos no Mato Grosso Mapa
-				 Cultural e divulgá-los gratuitamente.",
-
-            'home: agents' => "Você pode colaborar com a gestão da cultura do seu Estado e cidade
-				 com suas próprias informações preenchendo seu perfil de agente
-				 cultural. Neste espaço estão registrados artistas, gestores e
-				 produtores, uma rede de atores envolvidos na cena cultural 
-				mato-grossense. Você pode cadastrar um ou mais agentes (grupos,
-				 coletivos, bandas, empresas, instituições etc.), além de
-				 associar ao seu perfil eventos e espaços culturais com
-				 divulgação gratuita.",
-
-            'home: spaces' => "Encontre aqui espaços e equipamentos culturais de Mato Grosso
-				 incluídos na plataforma. Edificação, estrutura física ou área
-				 pública ou privada utilizada para o desenvolvimento de
-				 atividades culturais. Também bens imóveis e moveis que contenham
-				 valor arquitetônico histórico.",
-
-            'home: projects' => "Reúne projetos culturais ou agrupa eventos de todos os tipos.
-				 Neste espaço você encontra leis de fomento, mostras,
-				 convocatórias e editais criados pela SEC-MT, além de diversas
-				 iniciativas cadastradas pelos usuários da plataforma. 
-				 Cadastre-se e divulgue seus projetos.",
-
-            'home: home_devs' => 'Existem algumas maneiras de desenvolvedores interagirem com o
-				 Mapas Culturais. A primeira é através da nossa 
-				<a href="https://github.com/hacklabr/mapasculturais/blob/master/doc/api.md" target="_blank">API</a>. 
-				Com ela você pode acessar os dados públicos no nosso banco de dados e utilizá-los para desenvolver aplicações externas. 
-				Além disso, o Mapas Culturais é construído a partir do sofware livre <a href="http://institutotim.org.br/project/mapas-culturais/" target="_blank">Mapas Culturais</a>, criado em parceria com o <a href="http://institutotim.org.br" target="_blank">Instituto TIM</a>, e você pode contribuir para o seu desenvolvimento através do <a href="https://github.com/hacklabr/mapasculturais/" target="_blank">GitHub</a>.',
+            'home: title' => " <br> Bem-vind@!",
+            'home: abbreviation' => "MT",
+            'home: colabore' => "Colabore com o Mapas MT",
+            'home: welcome' => "<div style='width:50%; float:left'>Mapas MT é um software livre para mapeamento colaborativo. O sistema é alimentado tanto pelas Secretarias de Estados e Municípios os quais inserem na plataforma informações sobre os equipamentos culturais, turísticos, programações oficiais, editais, e outros, como pela população em geral, que se cadastra como agente (individual ou coletivo) e pode divulgar suas próprias programações. Esta ferramenta incorpora nos processos públicos as lógicas do software livre, da colaboração, da descentralização, do uso de dados abertos e da transparência. Além disso, fortalece o Plano Nacional e Estadual de Cultura, contribuindo para o seu cumprimento e acompanhamento de algumas de suas metas. Participe!
+.</div><div style='width:50%; float:left; margin-bottom:25px;'><iframe width='100%' height='250' src='https://www.youtube.com/embed/videoseries?list=PLJxEUm52CjMSH_byKXd14nlTdR0ZB2KeF' frameborder='0' allowfullscreen></iframe></div>",
+            'home: events' => "Veja a agenda de eventos da sua cidade. Como usuário cadastrado você pode incluir seus eventos no Mato Grosso Mapas MT e divulgá-los gratuitamente.",
+            'home: agents' => "Você pode colaborar com a gestão do seu Estado e Municípios com suas próprias informações preenchendo seu perfil de agente. Neste espaço estão registrados artistas, gestores e produtores, uma rede de atores envolvidos na cena cultural mato-grossense. Você pode cadastrar um ou mais agentes (grupos, coletivos, bandas, empresas, instituições etc.), além de associar ao seu perfil eventos e espaços culturais com divulgação gratuita.",
+            'home: spaces' => "Encontre aqui espaços e equipamentos culturais e turísticos de Mato Grosso incluídos na plataforma. Edificação, estrutura física ou área pública ou privada utilizada para o desenvolvimento de atividades. Também bens imóveis e moveis que contenham valor arquitetônico histórico.",
+            'home: projects' => "Reúne projetos culturais ou agrupa eventos de todos os tipos. Neste espaço você encontra leis de fomento, mostras, convocatórias e editais criados pelas Secretarias de Estado e Municípios, além de diversas iniciativas cadastradas pelos usuários da plataforma. Cadastre-se e divulgue seus projetos.",
+            'home: home_devs' => 'Existem algumas maneiras de desenvolvedores interagirem com o Mapas Culturais. A primeira é através da nossa <a href="https://github.com/hacklabr/mapasculturais/blob/master/doc/api.md" target="_blank">API</a>. Com ela você pode acessar os dados públicos no nosso banco de dados e utilizá-los para desenvolver aplicações externas. Além disso, o Cultura MT é construído a partir do sofware livre <a href="http://institutotim.org.br/project/mapas-culturais/" target="_blank">Mapas Culturais</a>, criado em parceria com o <a href="http://institutotim.org.br" target="_blank">Instituto TIM</a>, e você pode contribuir para o seu desenvolvimento através do <a href="https://github.com/hacklabr/mapasculturais/" target="_blank">GitHub</a>.',
 
             'search: verified results' => 'Resultados Verificados',
-
             'search: verified' => "Verificados"
         ];
     }
--- layouts/parts/header-about-nav.php
+++ layouts/parts/header-about-nav.php
@@ -1,7 +1,17 @@
 <nav id="about-nav" class="alignright clearfix">
-    <h1 id="organization-logo"><a href="#" target="_blank"><img src="<?php $this->asset('img/mapasculturais_logo-instituicao.svg'); ?>" /></a></h1>
+ 	<h1 id="organization-logo"><a href="#" target="_blank"><img src="<?php $this->asset('img/logoMT.png'); ?>"/></a></h1>    	
     <ul id="secondary-menu">
-        <li><a class="icon icon-about hltip" href="<?php echo $app->createUrl('site', 'page', array('sobre')) ?>" title="Sobre o Site"></a></li>
-        <li><a class="icon icon-help hltip" href="<?php echo $app->createUrl('site', 'page', array('como-usar')) ?>" title="Como usar"></a></li>
+        <li><h4><a class="icon icon-about hltip" href="<?php echo $app->createUrl('site', 'page', array('sobre')) ?>" title="Sobre o Site"></a></h4></li>
+        <li><h4><a class="icon icon-help hltip" href="<?php echo $app->createUrl('site', 'page', array('como-usar')) ?>" title="Como usar"></a></h4></li>
     </ul>
 </nav>
+<nav id="main-nav" class="clearfix">
+    <ul class="menu entiti1es-menu clearfix">
+        <li>
+            <a href="/como-usar/">
+                <div class="icon icon-manual"></div>
+                <div class="menu-item-label">Manual</div>
+            </a>
+        </li>
+    </ul>
+</nav> 
